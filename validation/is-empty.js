df1b3f10bd66317c10d4a518011795ccb7f2793c
er.rajatmalik <er.rajatmalik@gmail.com>
2020-03-28 19:33:57 +0530

    issue fix

--- validation/is-empty.js
+++ validation/is-empty.js
@@ -2,5 +2,5 @@ const isEmpty = value =>
   value === undefined ||
   value == null ||
   (typeof value === "object" && Object.keys(value).length === 0) ||
-  (typeof value === "string" && value.trin().length === 0);
+  (typeof value === "string" && value.trim().length === 0);
 module.exports = isEmpty;
