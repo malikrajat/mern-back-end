ce81d809af240196eebc568319659204f491f637
er.rajatmalik <er.rajatmalik@gmail.com>
2020-03-29 18:11:23 +0530

    update front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,27 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
 import "./App.css";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+        <div className="container">
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
+        </div>
+        <Footer />
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
