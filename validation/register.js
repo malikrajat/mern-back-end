1f2ee630e4776e4b22eb2020fd6d7020b9e0ca7d
er.rajatmalik <er.rajatmalik@gmail.com>
2020-03-31 13:08:33 +0530

    update registration validation

--- validation/register.js
+++ validation/register.js
@@ -2,47 +2,45 @@ const Validator = require("validator");
 const isEmpty = require("./is-empty");
 
 module.exports = function validateRegisterInput(data) {
-  let errors = {};
+  let error = {};
 
   data.name = !isEmpty(data.name) ? data.name : "";
   data.email = !isEmpty(data.email) ? data.email : "";
   data.password = !isEmpty(data.password) ? data.password : "";
   data.password2 = !isEmpty(data.password2) ? data.password2 : "";
-
   if (!Validator.isLength(data.name, { min: 2, max: 30 })) {
-    errors.name = "Name must be between 2 and 30 characters";
+    error.name = "Name must be between 2 and 30 characters";
   }
 
   if (Validator.isEmpty(data.name)) {
-    errors.name = "Name field is required";
+    error.name = "Name field is required";
   }
 
   if (Validator.isEmpty(data.email)) {
-    errors.email = "Email field is required";
+    error.email = "Email field is required";
   }
 
   if (!Validator.isEmail(data.email)) {
-    errors.email = "Email is invalid";
+    error.email = "Email is invalid";
   }
 
   if (Validator.isEmpty(data.password)) {
-    errors.password = "Password field is required";
+    error.password = "Password field is required";
   }
 
   if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
-    errors.password = "Password must be at least 6 characters";
+    error.password = "Password must be at least 6 characters";
   }
 
   if (Validator.isEmpty(data.password2)) {
-    errors.password2 = "Confirm Password field is required";
+    error.password2 = "Confirm Password field is required";
   }
 
   if (!Validator.equals(data.password, data.password2)) {
-    errors.password2 = "Passwords must match";
+    error.password2 = "Passwords must match";
   }
-
   return {
-    errors,
-    isValid: isEmpty(errors)
+    error,
+    isValid: isEmpty(error)
   };
 };
