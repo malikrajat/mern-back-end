1e14b7d7e783fbef4c1aebbb5fde1af518e43918
er.rajatmalik <er.rajatmalik@gmail.com>
2020-04-04 13:06:09 +0530

    add redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store/store";
 
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
@@ -10,17 +12,19 @@ import Login from "./components/auth/Login";
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Navbar />
-        <Route exact path="/" component={Landing} />
-        <div className="container">
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/register" component={Register} />
+    <Provider store={store}>
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <div className="container">
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/register" component={Register} />
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
-    </BrowserRouter>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
